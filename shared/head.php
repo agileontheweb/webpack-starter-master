d6dbc23c532187b3693662ae5c08b4fd894278f0
Alessandro Cuoghi <agileontheweb@gmail.com>
2018-06-28 12:35:35 +0200

    Added some opengraph.

--- shared/head.php
+++ shared/head.php
@@ -3,3 +3,7 @@
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.min.css" />
 <link rel="stylesheet" href="./dist/styles.css">
+
+<meta property="og:title" content="Title" />
+<meta property="og:url" content="http://www.miourl.com" />
+<meta property="og:image" content="http://www.miourl.com/images/myimage" />
