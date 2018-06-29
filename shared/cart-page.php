48163e973dea6716a171b358a15fb67914830090
Alessandro Cuoghi <agileontheweb@gmail.com>
2018-06-29 11:19:06 +0200

    Change some style

--- shared/cart-page.php
+++ shared/cart-page.php
@@ -5,50 +5,18 @@
         <p class="font-bold">Prima di procedere con l'acquisto controlla il tuo ordine.</p>
         <p class="text-sm">Se necessario puoi modificare le quantità, ricorda di aggiorna il carrello.</p>
       </div>
-      <!-- <div class="flex items-center bg-blue text-white text-sm font-bold px-4 py-3" role="alert">
-        <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.432 0c1.34 0 2.01.912 2.01 1.957 0 1.305-1.164 2.512-2.679 2.512-1.269 0-2.009-.75-1.974-1.99C9.789 1.436 10.67 0 12.432 0zM8.309 20c-1.058 0-1.833-.652-1.093-3.524l1.214-5.092c.211-.814.246-1.141 0-1.141-.317 0-1.689.562-2.502 1.117l-.528-.88c2.572-2.186 5.531-3.467 6.801-3.467 1.057 0 1.233 1.273.705 3.23l-1.391 5.352c-.246.945-.141 1.271.106 1.271.317 0 1.357-.392 2.379-1.207l.6.814C12.098 19.02 9.365 20 8.309 20z"/></svg>
-        <p>Prima di procedere con l'acquisto controlla il tuo ordine.</p>
-        <p class="text-sm">Some additional text to explain said message.</p>
-      </div> -->
+    </div>
 
-    </div>
     <div class="flex border mb-2">
       <div class="w-1/3 text-grey-darker py-2 m-2">
-        <a href="#" class="no-underline">
-          <img src="images/best-seller-1.png" />
-        </a>
+        <img src="images/best-seller-1.png" />
       </div>
       <div class="w-2/3 text-grey-darker py-2 m-2">
-        <h3 class="text-sm">PANSÉ -VIOLA DEL PENSIER</h3>
-        <p class="price text-sm pt-4 pb-4"><strong>€ 0,75</strong></p>
-      </div>
-      <div class="w-3/3 text-grey-darker py-2 m-2 text-center">
-        <input type="text" name="niente" id="niente" class="text-center content-center w-16 p-4 result-number border" value="1">
-      </div>
-    </div>
-    <div class="flex border mb-2">
-      <div class="w-1/3 text-grey-darker py-2 m-2">
-        <a href="#" class="no-underline">
-          <img src="images/best-seller-1.png" />
+        <a href="#" class="text-third no-underline">
+          <h3 class="text-sm">PANSÉ -VIOLA DEL PENSIER</h3>
         </a>
-      </div>
-      <div class="w-2/3 text-grey-darker py-2 m-2">
-        <h3 class="text-sm">PANSÉ -VIOLA DEL PENSIER</h3>
         <p class="price text-sm pt-4 pb-4"><strong>€ 0,75</strong></p>
-      </div>
-      <div class="w-3/3 text-grey-darker py-2 m-2 text-center">
-        <input type="text" name="niente" id="niente" class="text-center content-center w-16 p-4 result-number border" value="1">
-      </div>
-    </div>
-    <!-- <div class="flex border mb-2">
-      <div class="w-1/3 text-grey-darker py-2 m-2">
-        <a href="#" class="no-underline">
-          <img src="images/best-seller-1.png" />
-        </a>
-      </div>
-      <div class="w-2/3 text-grey-darker py-2 m-2">
-        <h3 class="text-sm">PANSÉ -VIOLA DEL PENSIER</h3>
-        <p class="price text-sm pt-4 pb-4"><strong>€ 0,75</strong></p>
+        <a href="#" class="text-red-lighter hover:text-red"><small>Rimuovi prodotto</small></a>
       </div>
       <div class="w-3/3 text-grey-darker py-2 m-2 text-center">
         <input type="text" name="niente" id="niente" class="text-center content-center w-16 p-4 result-number border" value="1">
@@ -56,13 +24,14 @@
     </div>
     <div class="flex border mb-2">
       <div class="w-1/3 text-grey-darker py-2 m-2">
-        <a href="#" class="no-underline">
-          <img src="images/best-seller-1.png" />
-        </a>
+        <img src="images/best-seller-1.png" />
       </div>
       <div class="w-2/3 text-grey-darker py-2 m-2">
-        <h3 class="text-sm">PANSÉ -VIOLA DEL PENSIER</h3>
+        <a href="#" class="text-third no-underline">
+          <h3 class="text-sm">PANSÉ -VIOLA DEL PENSIER</h3>
+        </a>
         <p class="price text-sm pt-4 pb-4"><strong>€ 0,75</strong></p>
+        <a href="#" class="text-red-lighter hover:text-red"><small>Rimuovi prodotto</small></a>
       </div>
       <div class="w-3/3 text-grey-darker py-2 m-2 text-center">
         <input type="text" name="niente" id="niente" class="text-center content-center w-16 p-4 result-number border" value="1">
@@ -70,24 +39,41 @@
     </div>
     <div class="flex border mb-2">
       <div class="w-1/3 text-grey-darker py-2 m-2">
-        <a href="#" class="no-underline">
-          <img src="images/best-seller-1.png" />
-        </a>
+        <img src="images/best-seller-1.png" />
       </div>
       <div class="w-2/3 text-grey-darker py-2 m-2">
-        <h3 class="text-sm">PANSÉ -VIOLA DEL PENSIER</h3>
+        <a href="#" class="text-third no-underline">
+          <h3 class="text-sm">PANSÉ -VIOLA DEL PENSIER</h3>
+        </a>
         <p class="price text-sm pt-4 pb-4"><strong>€ 0,75</strong></p>
+        <a href="#" class="text-red-lighter hover:text-red"><small>Rimuovi prodotto</small></a>
       </div>
       <div class="w-3/3 text-grey-darker py-2 m-2 text-center">
         <input type="text" name="niente" id="niente" class="text-center content-center w-16 p-4 result-number border" value="1">
       </div>
-    </div> -->
+    </div>
+    <div class="flex border mb-2">
+      <div class="w-1/3 text-grey-darker py-2 m-2">
+        <img src="images/best-seller-1.png" />
+      </div>
+      <div class="w-2/3 text-grey-darker py-2 m-2">
+        <a href="#" class="text-third no-underline">
+          <h3 class="text-sm">PANSÉ -VIOLA DEL PENSIER</h3>
+        </a>
+        <p class="price text-sm pt-4 pb-4"><strong>€ 0,75</strong></p>
+        <a href="#" class="text-red-lighter hover:text-red"><small>Rimuovi prodotto</small></a>
+      </div>
+      <div class="w-3/3 text-grey-darker py-2 m-2 text-center">
+        <input type="text" name="niente" id="niente" class="text-center content-center w-16 p-4 result-number border" value="1">
+      </div>
+    </div>
+
     <a href="#" class="block btn-update-order no-underline m-0">
       Aggiorna il carrello
     </a>
   </div>
 
-  <div class="xs:w-full sm:relative md:fixed sm:w-1/3 pt-2 p-4 pin-r bg-white">
+  <div class="xs:w-full sm:w-1/3 pt-2 p-4 pin-r bg-white">
     <div class="leading-normal mb-3">
       <h2 class="mt-3 mb-5">Procedi con il pagamento</h2>
       <div class="flex border-b text-xs py-3 mb-2 relative">
